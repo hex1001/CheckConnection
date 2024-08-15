131ef4ada3e069d9845b681fec2f8452a9b95e82
hex1001 <hex1001@mail.ru>
2024-08-15 20:57:12 +0500

    first commit

---
+++ CheckConnection/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace CheckConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(318, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::CheckConnection.Properties.Resources.d2b1ceb81eca460c5f3fb505ae36d92a1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ CheckConnection/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CheckConnection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //timer1.Enabled = true;
+            Check();
+
+        }
+        public async void Check()
+        {
+            while (true)
+            {
+                try
+                {
+                    using (var ping = new Ping())
+                    {
+                        var result1 = ping.Send("192.168.1.1", 200);
+                        var result2 = ping.Send("192.168.1.2", 200);
+                        if (result1.Status != IPStatus.Success && result2.Status != IPStatus.Success)
+                        {
+                            this.Show();
+                        }
+                        else
+                        {
+                            this.Hide();
+                        }
+                    }
+                }
+                catch
+                {
+                    ;
+                }
+                await Task.Delay(1000);
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var ping = new Ping())
+                {
+                    var result1 = ping.Send("192.168.1.1", 500);
+                    var result2 = ping.Send("192.168.1.2", 500);
+                    if (result1.Status != IPStatus.Success && result2.Status != IPStatus.Success)
+                    {
+                        this.Show();
+                    }
+                    else
+                    {
+                        this.Hide();
+                    }
+                }
+            }
+            catch
+            {
+                this.Hide();
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.Opacity = 100;
+            Hide();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //if (WindowState == FormWindowState.Minimized)
+            {
+                // Hide the form and display the NotifyIcon in the notification area
+                this.Opacity = 100;
+                Hide();
+                notifyIcon1.Visible = true;
+               
+            }
+        }
+    }
+}
